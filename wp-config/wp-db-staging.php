371a4cae921fddff797cd8bbbdd40dbe8f81066a
Zarah Sikora <zarahsikora1@Zarahs-MacBook-Pro.local>
2017-12-15 13:26:41 -0500

    initial commit

---
+++ wp-config/wp-db-staging.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('ABSPATH')) exit();
+
+define('DB_NAME', 'devontri_tff');
+define('DB_USER', 'devontri_devon');
+define('DB_PASSWORD', 'Forsaken_26!');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+ ?>
---
+++ wp-content/themes/known_starter/src/build/gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//VARS
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+//SCSS
+
+//look for every scss file in whatever directory we tell the function to look
+gulp.task('sass', function () {
+  return gulp.src('./../sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./../'));
+});
+
+//watch for the changes, tells us where to watch
+gulp.task('default', function () {
+  gulp.watch('./../sass/**/*.scss', ['sass']);
+});
+
+
+// JS
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    './bower_components/jquery/dist/jquery.min.js',
+    './../js/src/**/*.js'
+    ])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./../js/dist/'));
+});
+
+//auto watch
+gulp.task('watchScripts', function() {
+  gulp.watch('./../js/src/*.js', ['scripts'])
+});
