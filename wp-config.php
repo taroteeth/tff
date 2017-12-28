4f5bb7b71bb2349da0c0f331aff2b9e4fcf56453
Zarah Sikora <zarahsikora1@Zarahs-MacBook-Pro.local>
2017-12-28 11:29:22 -0500

    staging config

--- wp-config.php
+++ wp-config.php
@@ -18,9 +18,20 @@
  * @package WordPress
  */
 
- define('DB_NAME', 'tff');
- define('DB_USER', 'root');
- define('DB_PASSWORD', 'root');
+ //local
+
+ // define('DB_NAME', 'tff');
+ // define('DB_USER', 'root');
+ // define('DB_PASSWORD', 'root');
+ // define('DB_HOST', 'localhost');
+ // define('DB_CHARSET', 'utf8');
+ // define('DB_COLLATE', '');
+
+ // staging
+
+ define('DB_NAME', 'devontri_tff-z');
+ define('DB_USER', 'devontri_devon');
+ define('DB_PASSWORD', 'Forsaken_26!');
  define('DB_HOST', 'localhost');
  define('DB_CHARSET', 'utf8');
  define('DB_COLLATE', '');
