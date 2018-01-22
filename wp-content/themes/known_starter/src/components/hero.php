0200aae78f679a40f21b91911dcb916847d75d8c
Zarah Sikora <zarahsikora@gmail.com>
2018-01-22 10:13:15 -0500

    Network Module

--- wp-content/themes/known_starter/src/components/hero.php
+++ wp-content/themes/known_starter/src/components/hero.php
@@ -16,7 +16,7 @@ echo '<div id="hero">'; ?>
     if(!is_page('290')){ // there is no image in the hero for the resources page
       if($image){
         echo '<div id="hero-img-wrapper">';
-        echo wp_get_attachment_image($image);
+        echo wp_get_attachment_image($image, 'large');
         echo '</div> <!-- #hero-img-wrapper -->';
       }
     }
