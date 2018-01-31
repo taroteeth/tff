726a5dbf3f26198ccf2d620c0f2aa91599aa66d4
Zarah Sikora <zarahsikora@gmail.com>
2018-01-31 16:50:37 -0500

    Next steps half fixed

--- wp-content/themes/known_starter/src/components/next-steps.php
+++ wp-content/themes/known_starter/src/components/next-steps.php
@@ -1,14 +1,9 @@
 <?php
-if(get_post_type() == 'case_study'){
+if(get_post_type() == 'case_study' || get_post_type() == 'team_member'){
    $heading = get_field('next_step_heading');
    $image = get_field('next_step_image');
    $path = get_sub_field('next_step_page'[0]);
-}
-if(get_post_type() == 'team_member'){
-   $heading = get_field('next_step_heading');
-   $image = get_field('next_step_image');
-   $path = get_sub_field('next_step_page'[0]);
-} else {
+}  else {
    $heading = get_sub_field('next_step_heading');
    $image = get_sub_field('next_step_image');
    $path = get_sub_field('next_step_page'[0]);
@@ -37,7 +32,7 @@ if(get_post_type() == 'team_member'){
       ?>
     </div>
     <div class="image">
-      <img srcset="<?php echo wp_get_attachment_image($image, 'full'); ?>" src="<?php echo wp_get_attachment_image_url($image);?>" />
+      <?php echo wp_get_attachment_image($image, 'full'); ?>
       <svg class="photo-curve-horiz" viewBox="0 0 320 54"><use href="#photo-curve-horiz"></use></svg>
       <svg class="photo-curve-vert" viewBox="0 0 103 650"><use href="#photo-curve-vert"></use></svg>
     </div>
