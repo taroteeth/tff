9ab6c54d45ac21479d7bdc44a06a33396c3cbd9e
Devon Riley <devontriley@gmail.com>
2018-02-12 14:36:08 -0500

    fixed our clients grid

--- wp-content/themes/known_starter/src/components/clients-grid.php
+++ wp-content/themes/known_starter/src/components/clients-grid.php
@@ -1,81 +1,107 @@
-<?php
+<div class="clients-grid">
+  <div class="inner">
 
-echo '<div class="clients-grid">';
-echo '<div class="inner">';
+    <?php
 
-if( have_rows('client_industry_module' ) ) :
-  while( have_rows('client_industry_module' ) ) : the_row();
+    if( have_rows('client_industry_module' ) ) :
 
-    if( get_row_layout() == 'client_industry_module') :
+      while( have_rows('client_industry_module' ) ) : the_row();
 
-      $moduleTitle = get_sub_field('module_title');
+          $moduleTitle = get_sub_field('module_title');
+          $categories = get_sub_field_object('industry_category_submodule')['value'];
+          $categoriesArray = [];
+          foreach($categories as $cat){
+            $categoriesArray[] = $cat['category_title'];
+          }
+          ?>
 
-      echo '<div class="industry-module">';
+          <div class="industry-module">
 
-      echo '<p class="header">'. $moduleTitle .'</p>';
-      echo '<svg class="triangle" width="14px" height="7px" viewBox="0 0 14 7"><use href="#triangle-up"></use></svg>';
+          <p class="header"><?php echo $moduleTitle ?></p>
+          <svg class="triangle" width="14px" height="7px" viewBox="0 0 14 7"><use href="#triangle-up"></use></svg>
 
-      $categories = get_sub_field_object('industry_category_submodule')['value'];
-      $categoriesArray = [];
+          <div class="categories-list">
+          <?php foreach($categoriesArray as $catItem){ ?>
+              <span><?php echo $catItem ?></span>
+          <?php } ?>
+          </div><!-- categories-list -->
 
-      foreach($categories as $cat){
-        $categoriesArray[] = $cat['category_title'];
-      }
+          <?php
 
-      echo '<div class="categories-list">';
-        foreach($categoriesArray as $catItem){
-          echo '<span>'. $catItem .'</span>';
-        }
-      echo '</div><!-- categories-list -->';
+          if(have_rows('industry_category_submodule')) :
+            while(have_rows('industry_category_submodule')) : the_row();
 
+              $categoryTitle = get_sub_field('category_title');
+              ?>
 
-      if(have_rows('industry_category_submodule')) :
-        while(have_rows('industry_category_submodule')) : the_row();
-          $categoryTitle = get_sub_field('category_title');
+              <div class="category-submodule">
+                <p class="title"><?php echo $categoryTitle ?></p>
 
-          echo '<div class="category-submodule">';
-            echo '<p class="title">'. $categoryTitle .'</p>';
-            if( have_rows('category_exhibit') ) :
-              $iteration = 0;
-              $totalCount = 0;
-              $totalPosts = count(get_sub_field('category_exhibit'));
+                <?php
 
-              while( have_rows('category_exhibit') ) : the_row();
-              $investment = get_sub_field('investment_number');
-              $name = get_sub_field('client_name');
-              $location = get_sub_field('client_location');
-              $description = get_sub_field('client_description');
+                  $totalPosts = count(get_sub_field_object('category_exhibit')['value']);
 
-              if($iteration == 0) echo '<div class="row">';
+                if( have_rows('category_exhibit') ) :
 
-              echo '<div class="category-exhibit">';
-                if($investment) echo '<p class="investment">'. $investment .'</p>';
-                if($name) echo '<p class="name">'. $name .'</p>';
-                if($location) echo '<p class="location">'. $location .'</p>';
-                if($description) echo '<div class="description">'. $description .'</div>';
-              echo '</div><!-- .category-exhibit-->';
-              $iteration++;
+                  $iteration = 0;
+                  $totalCount = 0;
 
-              if($iteration == 3 || $totalCount == $totalPosts){
-                echo '</div><!-- .row -->';
-                $iteration = 0;
-              }
+                  while( have_rows('category_exhibit') ) : the_row();
 
-              endwhile;
-            endif; // category exhibit
+                    $investment = get_sub_field('investment_number');
+                    $name = get_sub_field('client_name');
+                    $location = get_sub_field('client_location');
+                    $description = get_sub_field('client_description');
+                    ?>
 
-          echo '</div><!-- .category-submodule-->';
-        endwhile; // category submodule
-      endif; //category submodule
+                    <?php if($iteration == 0) { ?>
+                      <div class="row">
+                    <?php } ?>
 
-      echo '</div><!-- .industry-module-->';
+                    <div class="category-exhibit">
+                      <?php if($investment) { ?>
+                        <p class="investment"><?php echo $investment ?></p>
+                      <?php } ?>
+                      <?php if($name) { ?>
+                        <p class="name"><?php echo $name ?></p>
+                      <?php } ?>
+                      <?php if($location) { ?>
+                        <p class="location"><?php echo $location ?></p>
+                      <?php } ?>
+                      <?php if($description) { ?>
+                        <div class="description"><?php echo $description ?></div>
+                      <?php } ?>
+                    </div><!-- .category-exhibit-->
 
-    endif; //get row layout
+                    <?php
+                    if($iteration == 2 || $totalCount == ($totalPosts - 1)) {
+                      $iteration = 0;
+                      ?>
+                      </div><!-- .row -->
+                    <?php
+                    } else {
+                      $iteration++;
+                    }
 
-endwhile; // have rows client industry module
-endif; // have rows client industry module
+                    $totalCount++;
 
-echo '</div><!-- .inner -->';
-echo '</div><!-- .clients-grid-->';
+                  endwhile;
+                endif; // category exhibit
+                ?>
 
-?>
+              </div><!-- .category-submodule-->
+
+            <?php
+            endwhile;
+          endif; //category submodule
+          ?>
+
+          </div><!-- .industry-module-->
+
+    <?php
+
+    endwhile; // have rows client industry module
+    endif; // have rows client industry module ?>
+
+  </div><!-- .inner -->
+</div><!-- .clients-grid-->
