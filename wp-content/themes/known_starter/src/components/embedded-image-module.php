3ad43bab6988b4b575bf3f7004818f89a7e871a1
Devon Riley <devontriley@gmail.com>
2018-02-20 10:07:50 -0500

    embedded image module

--- wp-content/themes/known_starter/src/components/embedded-image-module.php
+++ wp-content/themes/known_starter/src/components/embedded-image-module.php
@@ -1,5 +1,4 @@
 <?php
-
 $greyBg = get_sub_field('grey_background');
 $header = get_sub_field('header');
 $headerDivider = get_sub_field('header_underline');
@@ -8,34 +7,24 @@ $imageRound = get_sub_field('image_round');
 $imagePos = get_sub_field('image_position');
 $textContent = get_sub_field('text_content');
 $cta = get_sub_field('cta_option');
-
-echo '<div class="embed-image-module">';
-  echo '<div class="inner" >';
-    if($image){ ?>
-      <div class="embed-image-wrapper <?php if($imagePos === 'Left'){echo 'left';} if($imageRound){echo ', round';}?>"> <?php
-      echo wp_get_attachment_image($image, 'full');
-      echo '</div> <!-- .embed-image-wrapper -->';
-    } ?>
-
-    <div class="text-wrapper <?php if($imagePos === 'Left'){echo 'right';}?>"> <?php
-
-      if($header){ ?>
-        <p class="header <?php if($headerDivider){echo ', divider';}?>"> <?php
-        echo $header;
-        echo '</p>';
-      }
-
-      if($textContent){
-        echo '<div class="body-content">'. $textContent .'</div>';
-      }
-
-      if($cta){
-        include('button.php');
-      }
-
-    echo '</div><!-- .text-wrapper -->';
-    echo '</div><!-- .inner -->';
-
-echo '</div> <!-- .embed-image-module -->';
-
 ?>
+
+<div class="embedded-image-module">
+  <div class="alt-grid">
+    <div class="row-inner">
+      <div class="img-block color">
+        <?php echo wp_get_attachment_image($image, 'full'); ?>
+      </div>
+      <div class="text-block">
+        <div class="text-inner">
+          <div class="text-header fade_in">
+            <p><?php echo $header ?></p>
+          </div>
+          <div class="text-copy fade_in">
+            <p><?php echo $textContent ?></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
