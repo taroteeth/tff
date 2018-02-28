e9c5739163e17ba085792a0c4c3c3c750c8c0b01
Zarah Sikora <zarahsikora@gmail.com>
2018-02-28 11:36:13 -0500

    404

--- wp-content/themes/known_starter/src/404.php
+++ wp-content/themes/known_starter/src/404.php
@@ -1,22 +1,10 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<div class="404-wrapper">
+<div class="notfound-wrapper">
 
-			<!-- article -->
-			<article id="post-404">
+	<p class="header"><?php _e( 'Page not found', 'html5blank' ); ?></p>
+	<a class="home-link" href="<?php echo home_url(); ?>"><?php _e( 'Return home', 'html5blank' ); ?></a>
 
-				<p class="header"><?php _e( 'Page not found', 'html5blank' ); ?></p>
-				<a class="home-link" href="<?php echo home_url(); ?>"><?php _e( '< Return home', 'html5blank' ); ?></a>
-
-			</article>
-			<!-- /article -->
-
-		</div>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+</div>
 
 <?php get_footer(); ?>
