e4073bc906616d78a078ca9437f87ebb0d84ddf7
Zarah Sikora <zarahsikora@gmail.com>
2018-03-06 11:09:38 -0500

    font fix

--- wp-content/themes/known_starter/src/header.php
+++ wp-content/themes/known_starter/src/header.php
@@ -9,9 +9,6 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-		<!-- <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,400i,700" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700" rel="stylesheet"> -->
-
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
